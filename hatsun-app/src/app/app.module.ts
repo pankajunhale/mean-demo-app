4975447b2c03665a7093c01ce5e78d352507eeb6
Shubham Rajput <shubhamrajput12380@gmail.com>
2020-11-02 18:20:43 +0530

    Commented sample component

--- hatsun-app/src/app/app.module.ts
+++ hatsun-app/src/app/app.module.ts
@@ -5,11 +5,13 @@ import { SampleService } from './services/sample.service';
 import { AppRoutingModule } from './app-routing-module';
 import { SampleViewComponent } from './sample/view/sample-view/sample-view.component';
 import { HttpClientModule } from '@angular/common/http';
+import { LoginPageComponent } from './login-page/login-page.component';
 
 @NgModule({
   declarations: [
     AppComponent,
-    SampleViewComponent
+    SampleViewComponent,
+    LoginPageComponent
   ],
   imports: [
     BrowserModule,
