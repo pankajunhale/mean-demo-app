e43afcdf04205e44bf0e0ab1870b5d68454effd0
shubham261996 <tagadechinmay@gmail.com>
2020-11-06 14:42:47 +0530

    Added new component name as user-master-list

---
+++ hatsun-app/src/app/user-master-list/user-master-list.component.ts
@@ -0,0 +1,15 @@
+import { Component, OnInit } from '@angular/core';
+
+@Component({
+  selector: 'app-user-master-list',
+  templateUrl: './user-master-list.component.html',
+  styleUrls: ['./user-master-list.component.css']
+})
+export class UserMasterListComponent implements OnInit {
+
+  constructor() { }
+
+  ngOnInit() {
+  }
+
+}
