223d7fa0f75b451065e579a3c645a78f6d1597a7
shreyasunhale0110 <shreyasunhale0110@gmail.com>
2020-11-12 14:22:44 +0530

    fixed an issue where menu was seen at login screen

--- hatsun-app/src/app/login-page/login-page.component.ts
+++ hatsun-app/src/app/login-page/login-page.component.ts
@@ -3,41 +3,44 @@ import { AppComponent } from '../app.component';
 import { UserService } from '../services/user.service';
 import { Router } from '../../../node_modules/@angular/router';
 import { FormGroup, FormControl, Validators } from '@angular/forms';
+import { CommonService } from '../services/common.service';
+//import { win32 } from 'path';
+import { BaseComponent } from '../model/base.component';
 
 @Component({
   selector: 'app-login-page',
   templateUrl: './login-page.component.html',
   styleUrls: ['./login-page.component.css']
 })
-export class LoginPageComponent implements OnInit {
+export class LoginPageComponent extends BaseComponent implements OnInit {
 
   loginForm = new FormGroup({
     userEmail: new FormControl(null, [Validators.required]),
     userPassword: new FormControl(null, [Validators.required]),
   });
-  constructor(private router: Router,private userService: UserService) { 
-    
+  constructor(private router: Router,private userService: UserService, private commonService: CommonService) { 
+    super();
+    this.init();
   }
 
   ngOnInit() {
   }
 
-  login() {
-    this.router.navigate(['/home'])
-  }
 
   auth(){
-    console.log(this.loginForm.get('userEmail').value);
-
     this.userService.authenticate(this.loginForm.get('userEmail').value, this.loginForm.get('userPassword').value).subscribe((response) =>{
-     //alert('Login Successful')
-      this.router.navigate(['/userList'])
-      debugger;
+      this.commonService.setLocalStorageItem('isLoggedIn', '1');
+      // this.router.navigate(['/userList']); //hack
+      window.location.href = '/userList'
     },()=>{
-      debugger;
       alert('Invalid credentials')
     })
   }
 
+  private init(){
+    if(this.commonService.isUserLoggedIn(this.IS_LOGGED_IN)){
+      this.commonService.redirectToPath('/userList',true);
+    }
+  }
   get getUserFormRef() { return this.loginForm.controls }
 }
